564d8dec66f15b42585c8eebf3a9505bd15707a3
Ephraim Becker <emb16@outlook.com>
2022-12-29 11:39:22 -0500

    Added landing barebones.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./scenes/Navbar";
 import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
 import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 
@@ -19,21 +20,24 @@ function App() {
     return () => window.removeEventListener("scroll", handleScroll);
   })
 
-  return <div className="app bg-deep-blue">
-    <Navbar
-      isTopOfPage={isTopOfPage}
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
-    />
-    <div className="w-5/6 mx-auto md:h-full">
-      {isAboveMediumScreens && (
-        <DotGroup
-          selectedPage={selectedPage}
-          setSelectedPage={setSelectedPage}
-        />
-      )}
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default App;
