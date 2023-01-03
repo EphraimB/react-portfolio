c311dde4adaf38465547ed8ab5acd3a0407866fb
Ephraim Becker <emb16@outlook.com>
2023-01-03 14:04:33 -0500

    Added brush image.

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -21,13 +21,37 @@ const Landing = ({ setSelectedPage }) => {
                     </div>
                 ) : (
                     <img
-                            alt="profile"
-                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-                            src={ProfileImage}
-                        />
+                        alt="profile"
+                        className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+                        src={ProfileImage}
+                    />
                 )}
             </div>
-        </section>
+            {/* MAIN SECTION */}
+            <div className="z-30 basis-2/5 mt-12 md:mt-32">
+                {/* HEADINGS */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                        Jane {""}
+                        <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]">
+                            Esper
+                        </span>
+                    </p>
+                    <p className="mt-10 mb-7 text-sm text-center md:text-start">
+                        Just a placeholder name
+                    </p>
+                </motion.div>
+            </div>
+        </section >
     )
 }
 
