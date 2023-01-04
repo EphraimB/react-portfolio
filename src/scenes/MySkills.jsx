b00d224923564f4797e251a8a03fa1e19744304b
Ephraim Becker <emb@paay.co>
2023-01-04 16:15:06 -0500

    Added skills page.

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -1,6 +1,7 @@
 import LineGradient from "../components/LineGradient";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
+import SkillsImage from "../assets/skills-image.png";
 
 const MySkills = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -14,7 +15,7 @@ const MySkills = () => {
                     initial="hidden"
                     whileInView="visible"
                     viewport={{ once: true, amount: 0.5 }}
-                    transition={{ delay: 0.4, duration: 0.5 }}
+                    transition={{ duration: 0.5 }}
                     variants={{
                         hidden: { opacity: 0, x: -50 },
                         visible: { opacity: 1, x: 0 },
@@ -26,6 +27,104 @@ const MySkills = () => {
                     <LineGradient width="w-1/3" />
                     <p className="mt-10 mb-7">
                         Lorem ipsum
+                    </p>
+                </motion.div>
+
+                <div className="mt-16 md:mt-0">
+                    {isAboveMediumScreens ? (
+                        <div
+                            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-10 before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+                            <img
+                                alt="skills"
+                                className="z-10"
+                                src={SkillsImage}
+                            />
+                        </div>
+                    ) : (
+                        <img
+                            alt="skills"
+                            className="z-10"
+                            src={SkillsImage}
+                        />
+                    )}
+                </div>
+            </div>
+
+            {/* SKILLS */}
+            <div className="md:flex md:justify-between mt-16 gap-32">
+                {/* EXPERIENCE */}
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                >
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">01</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">
+                                Experience
+                            </p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+                    </div>
+                    <p className="mt-5">
+                        Just some stupid gibberish in your cool life.
+                    </p>
+                </motion.div>
+
+                {/* Innovative */}
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                >
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">02</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">
+                                Innovative
+                            </p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+                    </div>
+                    <p className="mt-5">
+                        Just some stupid gibberish in your cool life.
+                    </p>
+                </motion.div>
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                >
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">03</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">
+                                Imaginative
+                            </p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+                    </div>
+                    <p className="mt-5">
+                        Just some stupid gibberish in your cool life.
                     </p>
                 </motion.div>
             </div>
