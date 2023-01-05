60d5b541b2067058434c9881cb14736c8ef526d5
Ephraim Becker <emb@paay.co>
2023-01-05 14:25:31 -0500

    Added the projects.

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,5 +1,12 @@
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
+import project1 from "../assets/project-1.jpeg";
+import project2 from "../assets/project-2.jpeg";
+import project3 from "../assets/project-3.jpeg";
+import project4 from "../assets/project-4.jpeg";
+import project5 from "../assets/project-5.jpeg";
+import project6 from "../assets/project-6.jpeg";
+import project7 from "../assets/project-7.jpeg";
 
 const container = {
     hidden: {},
@@ -13,8 +20,21 @@ const projectVariant = {
     visible: { opacity: 1, scale: 1 }
 }
 
-const Project = ({ title }) => {
-    const projectTitle = title.split(" ").join("-").toLowerCase();
+const Project = ({ title, image }) => {
+    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    const projectTitle = title.split(" ").join("").toLowerCase();
+
+    return (
+        <motion.div variants={projectVariant} className="relative">
+            <div className={overlayStyles}>
+                <p className="text-2xl font-playfair">{title}</p>
+                <p className="mt-7">
+                    Lorem ipsum
+                </p>
+            </div>
+            <img src={image} alt={projectTitle} />
+        </motion.div>
+    )
 }
 
 const Projects = () => {
@@ -60,7 +80,17 @@ const Projects = () => {
                         className="flex justify-center text-center items-center p-10 bg-red max-w-[400px] max--[400px] text-2xl font-playfair font-semibold">
                         BEAUTIFUL USER INTERFACES
                     </div>
-                    <Project title="Page 1" />
+                    <Project title="Project 1" image={project1} />
+                    <Project title="Project 2" image={project2} />
+                    <Project title="Project 3" image={project3} />
+                    <Project title="Project 4" image={project4} />
+                    <Project title="Project 5" image={project5} />
+                    <Project title="Project 6" image={project6} />
+                    <Project title="Project 7" image={project7} />
+                    <div
+                        className="flex justify-center text-center items-center p-10 bg-blue max-w-[400px] max--[400px] text-2xl font-playfair font-semibold">
+                        SMOOTH USER EXPERIENCE
+                    </div>
                 </motion.div>
             </div>
         </section>
